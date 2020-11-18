d1767ac8b041db5741b77e01efae6220362bdad4
Patrick Aubin <p_aubin@apple.com>
2020-11-18 11:09:14 -0800

    modernized status

--- halffull/settings.py
+++ halffull/settings.py
@@ -119,7 +119,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/halffull/site/halffull/templates',
+    'templates',
 )
 
 INSTALLED_APPS = (
